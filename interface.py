3ff87fae9741cf43e38ddc7fed0a661abda76e64
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2022-12-12 02:59:55 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -23,27 +23,28 @@ st.write("By: Akhmad Amanulloh (20041110099)")
 deskripsi,dataframe, preporcessing, modeling, implementation = st.tabs(
     ["Deskripsi","Data", "Prepocessing", "Modeling", "Implementation"])
 with deskripsi:
-    st.subheader("""Tentang Dataset""")
-    st.write("Penyakit kardiovaskular (CVDs) adalah penyebab kematian nomor 1 secara global , merenggut sekitar 17,9 juta nyawa setiap tahun , yang merupakan 31% dari semua kematian di seluruh dunia . Gagal jantung adalah kejadian umum yang disebabkan oleh CVD dan kumpulan data ini berisi 12 fitur yang dapat digunakan untuk memprediksi kematian akibat gagal jantung.")
-    st.write("Sebagian besar penyakit kardiovaskular dapat dicegah dengan mengatasi faktor risiko perilaku seperti penggunaan tembakau, pola makan yang tidak sehat dan obesitas, kurangnya aktivitas fisik, dan penggunaan alkohol yang berbahaya dengan menggunakan strategi populasi luas.")
-    st.write("Orang dengan penyakit kardiovaskular atau yang memiliki risiko kardiovaskular tinggi (karena adanya satu atau lebih faktor risiko seperti hipertensi, diabetes, hiperlipidemia, atau penyakit yang sudah ada) memerlukan deteksi dan penanganan dini di mana model pembelajaran mesin dapat sangat membantu.")
-    st.write("""Dataset gagal jantung ini diambil dari <a href="https://www.kaggle.com/datasets/andrewmvd/heart-failure-clinical-data">Kaggle</a>""", unsafe_allow_html=True)
-    st.subheader("""Fitur""")
-    st.write(
-        """
-        Fitur yang terdapat pada dataset:
-        - Age : Usia pasien
-        - Anaemia : Masalah kesehatan yang terjadi saat jumlah sel darah merah dalam tubuh lebih rendah dibandingkan dengan jumlah normalnya, sering dikenal dengan penyakit kekurangan sel darah merah
-        - Creatinine Phosphokinase: Creatine Phosphokinase (CK) adalah sejenis protein yang dikenal sebagai enzim. Protein tersebut sebagian besar ditemukan di otot rangka dan jantung, dengan jumlah yang lebih sedikit di otak. Sedangkan tes CK digunakan untuk mengukur jumlah creatine kinase dalam darah. 
-        - Diabetes: Diabetes atau penyakit gula adalah penyakit kronis atau yang berlangsung jangka panjang. Penyakit ini ditandai dengan meningkatnya kadar gula darah (glukosa) hingga di atas nilai normal.
-        - Ejection Fraction: Dalam pemeriksaan USG jantung, yang paling penting dinilai ialah nilai fraksi ejeksi / ejection fraction (EF). Fraksi ejeksi mencerminkan seberapa banyak darah yang terpompa dibandingkan dengan jumlah darah yang masuk ke dalam kamar jantung. Normalnya, nilai EF berkisar antara 50-70%.
-        - High Blood Pressure: Tekanan darah tinggi atau disebut juga hipertensi adalah suatu kondisi ketika seseorang mempunyai tekanan darah yang terukur pada nilai 130/80 mmHg atau lebih tinggi.
-        - Platelets: Trombosit (keping darah/platelet) adalah komponen darah yang berfungsi dalam pembekuan darah. Jumlahnya yang terlalu rendah dapat membuat Anda mudah memar dan mengalami perdarahan.
-        - Serum Creatinine: Serum kreatinin merupakan sampah hasil metabolisme otot yang mengalir pada sirkulasi darah. Kreatinin lalu disaring ginjal untuk selanjutnya dibuang bersama urine. Serum kreatinin menjadi pertanda baik buruknya fungsi ginjal, karena organ ini yang mengatur agar kreatinin tetap berada pada kadar normalnya.
-        - Serum Sodium: Kadar natrium serum adalah parameter utama yang digunakan untuk menilai tonisitas serum yang sering terganggu akibat hiperglikemia. Efek hiperglikemia terhadap penurunan konsentrasi natrium plasma telah diketahui sejak separuh abad yang lalu.
-        - Sex: Jenis Kelamin 
-        """
-    )
+    st.subheader("Deskripsi")
+    st.write("Nama : Akhmad Amanulloh | NIM : 200411100099 | Kelas : Penambangan Data A")
+    st.write("")
+    st.write("Dataset berisi tentang cuaca yang tepat untuk bermain tennis.")
+    st.write("Aplikasi ini digunakan untuk cuaca yang tepat untuk bermain tennis.").")
+    st.write("Fitur yang digunakan :")
+    st.write("1. Outlook (Pandangan) : Kategorikal")
+    st.write("sunny=cerah")
+    st.write("overcast=mendung")
+    st.write("rainy=hujan")
+    st.write("2. gender (Jenis kelamin) : Kategorikal")
+    st.write("3. height_cm (Tinggi Badan) : Numerik")
+    st.write("4. weight_kg (Berat badan) : Numerik")
+    st.write("5. body_fat (Lemak tubuh) : Numerik")
+    st.write("6. diastolic (Tekanan darah diastolic) : Numerik")
+    st.write("7. systolic (Tekanan darah systolic) : Numerik")
+    st.write("8. gripForce (Kekuatan cengkrama) : Numerik")
+    st.write("9. sit_and_binding_fowrard (Jarak sitting forward binding : Numerik")
+    st.write("10. sit_ups_count (Jumlah sit up) : Numerik")
+    st.write("11. broad_jumps_cm (Nilai lompat jauh  : Numerik")
+    st.write("Sumber dataset https://www.kaggle.com/datasets/kukuroo3/body-performance-data")
+    st.write("Link github https://github.com/alaziz31/interface")
 with dataframe:
     url = "https://www.kaggle.com/datasets/pranavpandey2511/tennis-weather"
     st.markdown(
@@ -116,7 +117,7 @@ with dataframe:
             st.success(f'akurasi terhadap data test = {akurasi_d3}')
             st.dataframe(label_d3)
     with implementation:
-        tema = st.selectbox('Temperatur', ['cool', 'hot', 'mild'])
+        tema = st.selectbox('Suhu', ['cool', 'hot', 'mild'])
         temp_cool = 1 if tema == 'cool' else 0
         temp_hot = 1 if tema == 'hot' else 0
         temp_mild = 1 if tema == 'mild' else 0
