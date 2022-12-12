b5463a00cee6398242b4559917fb52503a417caf
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2022-12-12 08:21:22 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -16,37 +16,13 @@ from sklearn.naive_bayes import GaussianNB
 
 # display
 # st.set_page_config(layout='wide')
-st.set_page_config(page_title="Weather Play Tennis")
+st.set_page_config(page_title="weather play tennis")
 
-st.title("weather play tennis")
-deskripsi,dataframe, preporcessing, modeling, implementation = st.tabs(
-    ["Deskripsi","Data", "Prepocessing", "Modeling", "Implementation"])
-with deskripsi:
-    st.subheader("Deskripsi")
-    st.write("Nama : Akhmad Amanulloh | NIM : 200411100099 | Kelas : Penambangan Data A")
-    st.write("")
-    st.write("Dataset berisi tentang cuaca yang tepat untuk bermain tennis.")
-    st.write("Aplikasi ini digunakan untuk cuaca yang tepat untuk bermain tennis.")
-    st.write("Fitur yang digunakan :")
-    st.write("1. Outlook (Pandangan) : Kategorikal")
-    st.write("sunny=cerah")
-    st.write("overcast=mendung")
-    st.write("rainy=hujan")
-    st.write("2. temp (suhu) : Kategorikal")
-    st.write("hoot=panas")
-    st.write("mild=normal")
-    st.write("cool=dingin")
-    st.write("3. humidity (kelembapan) : Kategorikal")
-    st.write("high=tinggi")
-    st.write("normal=normal")
-    st.write("4. windy (berangin) : boolean")
-    st.write("true=benar")
-    st.write("false=salah")
-    st.write("5. play (main) : boolean")
-    st.write("yes=iya")
-    st.write("no=tidak")
-    st.write("Sumber dataset https://www.kaggle.com/datasets/pranavpandey2511/tennis-weather")
-    st.write("Link github https://github.com/akhmadamanulloh/aplikasidatamining/")
+st.title("UAS PENDAT")
+st.write("By: Akhmad Amanulloh (20041110099)")
+dataframe, preporcessing, modeling, implementation = st.tabs(
+    ["Data", "Prepocessing", "Modeling", "Implementation"])
+
 with dataframe:
     url = "https://www.kaggle.com/datasets/pranavpandey2511/tennis-weather"
     st.markdown(
@@ -65,87 +41,70 @@ with dataframe:
                     """)
         with preporcessingg:
             st.subheader("Preprocessing")
-            #Mendefinisikan Varible X dan Y
+            dt['outlook'].unique()
             dt_dum=pd.get_dummies(data=dt,columns=['temp','outlook','humidity','windy'])
             dt_dum
-            X = dt_dum.drop(columns=['play'])
-            y = dt_dum['play'].values
-            dt_min = X.min()
-            dt_max = X.max()
+    with modeling:
+        X = dt_dum.drop('play',axis=1)
+        y = dt_dum['play']
+        # split data
+        X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.5,random_state=1)
+        clf, knc, dtc = st.tabs(
+        ["GaussianNB", "KNeighborsClassifier", "DecisionTreeClassifier"])
+        with clf:
+            clf = GaussianNB(priors=None)
+            clf.fit(X_train,y_train)
+            y_pred_clf = clf.predict(X_test)
+            akurasi_clf = accuracy_score(y_test, y_pred_clf)
+            label_clf = pd.DataFrame(
+            data={'Label Test': y_test, 'Label Predict': y_pred_clf}).reset_index()
+            st.success(f'akurasi terhadap data test = {akurasi_clf}')
+            st.dataframe(label_clf)
+        with knc:
+            knn = KNeighborsClassifier(n_neighbors = 5)
+            knn.fit(X_train,y_train)
+            y_pred_knn = knn.predict(X_test)
+            akurasi_knn = accuracy_score(y_test, y_pred_knn)
+            label_knn = pd.DataFrame(
+            data={'Label Test': y_test, 'Label Predict': y_pred_knn}).reset_index()
+            st.success(f'akurasi terhadap data test = {akurasi_knn}')
+            st.dataframe(label_knn)
+        with dtc:
+            classifier=DecisionTreeClassifier(criterion='gini')
+            classifier.fit(X_train,y_train)
+            y_pred_d3 = classifier.predict(X_test)
+            akurasi_d3 = accuracy_score(y_test, y_pred_d3)
+            label_d3 = pd.DataFrame(
+            data={'Label Test': y_test, 'Label Predict': y_pred_d3}).reset_index()
+            st.success(f'akurasi terhadap data test = {akurasi_d3}')
+            st.dataframe(label_d3)
+    with implementation:
+        tema = st.selectbox('Temperatur', ['cool', 'hot', 'mild'])
+        temp_cool = 1 if tema == 'cool' else 0
+        temp_hot = 1 if tema == 'hot' else 0
+        temp_mild = 1 if tema == 'mild' else 0
 
-            #NORMALISASI NILAI X
-            scaler = MinMaxScaler()
-            scaled = scaler.fit_transform(X)
-            features_names = X.columns.copy()
-            scaled_features = pd.DataFrame(scaled, columns=features_names)
+        outlook = st.selectbox('outlook', ['overcast', 'rainy', 'sunny'])
+        outlook_overcast = 1 if outlook == 'overcast' else 0
+        outlook_rainy = 1 if outlook == 'rainy' else 0
+        outlook_sunny = 1 if outlook == 'sunny' else 0
 
-            st.subheader('Hasil Normalisasi Data')
-            st.write(scaled_features)
+        humidity = st.selectbox('humidity', ['high', 'normal'])
+        humidity_high = 1 if humidity == 'high' else 0
+        humidity_normal = 1 if humidity == 'normal' else 0
 
-            st.subheader('Target Label')
-            dumies = pd.get_dummies(dt.play).columns.values.tolist()
-            dumies 
-        with modeling:
-            # split data
-            X_train, X_test, y_train, y_test = train_test_split(
-            X, y, train_size=0.45,random_state=1)
-            clf, knc, dtc = st.tabs(
-            ["GaussianNB", "KNeighborsClassifier", "DecisionTreeClassifier"])
-            with clf:
-                clf = GaussianNB(priors=None)
-                clf.fit(X_train,y_train)
-                y_pred_clf = clf.predict(X_test)
-                akurasi_clf = accuracy_score(y_test, y_pred_clf)
-                label_clf = pd.DataFrame(
-                data={'Label Test': y_test, 'Label Predict': y_pred_clf}).reset_index()
-                st.success(f'akurasi terhadap data test = {akurasi_clf}')
-                st.dataframe(label_clf)
-            with knc:
-                knn = KNeighborsClassifier(n_neighbors = 10)
-                knn.fit(X_train,y_train)
-                y_pred_knn = knn.predict(X_test)
-                akurasi_knn = accuracy_score(y_test, y_pred_knn)
-                label_knn = pd.DataFrame(
-                data={'Label Test': y_test, 'Label Predict': y_pred_knn}).reset_index()
-                st.success(f'akurasi terhadap data test = {akurasi_knn}')
-                st.dataframe(label_knn)
-            with dtc:
-                classifier=DecisionTreeClassifier(criterion='gini')
-                classifier.fit(X_train,y_train)
-                y_pred_d3 = classifier.predict(X_test)
-                akurasi_d3 = accuracy_score(y_test, y_pred_d3)
-                label_d3 = pd.DataFrame(
-                data={'Label Test': y_test, 'Label Predict': y_pred_d3}).reset_index()
-                st.success(f'akurasi terhadap data test = {akurasi_d3}')
-                st.dataframe(label_d3)
-        with implementation:
-            tema = st.selectbox('Temperatur', ['cool', 'hot', 'mild'])
-            temp_cool = 1 if tema == 'cool' else 0
-            temp_hot = 1 if tema == 'hot' else 0
-            temp_mild = 1 if tema == 'mild' else 0
+        windy = st.selectbox('windy', ['False', 'True'])
+        windy_False = 1 if windy == 'False' else 0
+        windy_True = 1 if windy == 'True' else 0
 
-            outlook = st.selectbox('outlook', ['overcast', 'rainy', 'sunny'])
-            outlook_overcast = 1 if outlook == 'overcast' else 0
-            outlook_rainy = 1 if outlook == 'rainy' else 0
-            outlook_sunny = 1 if outlook == 'sunny' else 0
-
-            humidity = st.selectbox('humidity', ['high', 'normal'])
-            humidity_high = 1 if humidity == 'high' else 0
-            humidity_normal = 1 if humidity == 'normal' else 0
-
-            windy = st.selectbox('windy', ['False', 'True'])
-            windy_False = 1 if windy == 'False' else 0
-            windy_True = 1 if windy == 'True' else 0
-
-            data = np.array([[temp_cool,temp_hot,temp_mild,outlook_overcast,outlook_rainy,outlook_sunny,humidity_high,humidity_normal,windy_False,windy_True]])
-            model = st.selectbox('Pilih Model', ['GaussianNB', 'KNeighborsClassifier', 'DecisionTreeClassifier'])
-            if model == 'GaussianNB':
-                y_imp = clf.predict(data)
-            elif model == 'KNeighborsClassifier':
-                y_imp = knn.predict(data)
-            else:
-                y_imp = classifier.predict(data)
-            st.success(f'Model yang dipilih = {model}')
-            st.success(f'Data Predict = {y_imp}')
-   
-            
+        data = np.array([[temp_cool,temp_hot,temp_mild,outlook_overcast,outlook_rainy,outlook_sunny,humidity_high,humidity_normal,windy_False,windy_True]])
+        model = st.selectbox('Pilih Model', ['GaussianNB', 'KNeighborsClassifier', 'DecisionTreeClassifier'])
+        if model == 'GaussianNB':
+            y_imp = clf.predict(data)
+        elif model == 'KNeighborsClassifier':
+            y_imp = knn.predict(data)
+        else:
+            y_imp = classifier.predict(data)
+        st.success(f'Model yang dipilih = {model}')
+        st.success(f'Data Predict = {y_imp}')
